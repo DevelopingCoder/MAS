9c00049250dcd9f582dcbdc8a67b4b7f190a87f6
Anthony Tan <atan5133@berkeley.edu>
2015-12-06 03:49:51 -0800

    updated to reflect Evan's output

--- concatenateFiles/src/concatenateFiles/FileConcatenator.java
+++ concatenateFiles/src/concatenateFiles/FileConcatenator.java
@@ -7,23 +7,27 @@ public class FileConcatenator {
 		 * into outputs/testResults.out
 		 */
 		int Ashley = 0;
+		int Evan = 0;
 		String[] bestResultsArray = new String[622];
 		for(int i = 1; i < 622; i++) {
 			File[] files = new File[4];
 			files[0] = new File("outputs" + "1/" + i + ".out");
 			files[1] = new File("outputs" + "2/" + i + ".out");
-//			files[2] = new File("outputs" + "3/" + i + ".out");
+			files[2] = new File("outputs" + "3/" + i + ".out");
 //			files[3] = new File("outputs" + "4/" + i + ".out");
 			
 			BufferedReader output;
 			int bestResultsCount = -1;
-			for(int j = 0; j < 2; j++) {
+			for(int j = 0; j < 3; j++) {
 				output = new BufferedReader(new FileReader(files[j]));	
 				String edges = output.readLine();
 				int count = Integer.parseInt(edges);
 				if (count > bestResultsCount) {
 					if (j == 1) {
 						Ashley += 1;
+					}
+					if (j==2) {
+						Evan += 1;
 					}
 					bestResultsCount = count;
 					bestResultsArray[i] = output.readLine();
@@ -37,6 +41,7 @@ public class FileConcatenator {
 			out.println(bestResultsArray[i]);
 		}
 		out.close();
-		System.out.print(Ashley);
+		System.out.println(Ashley);
+		System.out.println(Evan);
 	}
 }
