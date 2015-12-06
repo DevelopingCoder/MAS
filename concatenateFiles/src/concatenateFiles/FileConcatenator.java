9eeb7d057360bc54a729ee5fb4748459c1c5325f
Anthony Tan <atan5133@berkeley.edu>
2015-12-06 03:03:33 -0800

    submitting for phase 2

--- concatenateFiles/src/concatenateFiles/FileConcatenator.java
+++ concatenateFiles/src/concatenateFiles/FileConcatenator.java
@@ -6,21 +6,25 @@ public class FileConcatenator {
 		/*Read the output files from each folder and compare them. Find the best one, and write it
 		 * into outputs/testResults.out
 		 */
-		String[] bestResultsArray = new String[10];
-		for(int i = 1; i < 10; i++) {
+		int Ashley = 0;
+		String[] bestResultsArray = new String[622];
+		for(int i = 1; i < 622; i++) {
 			File[] files = new File[4];
 			files[0] = new File("outputs" + "1/" + i + ".out");
 			files[1] = new File("outputs" + "2/" + i + ".out");
-			files[2] = new File("outputs" + "3/" + i + ".out");
-			files[3] = new File("outputs" + "4/" + i + ".out");
+//			files[2] = new File("outputs" + "3/" + i + ".out");
+//			files[3] = new File("outputs" + "4/" + i + ".out");
 			
 			BufferedReader output;
-			int bestResultsCount = 0;
-			for(int j = 0; j < 4; j++) {
+			int bestResultsCount = -1;
+			for(int j = 0; j < 2; j++) {
 				output = new BufferedReader(new FileReader(files[j]));	
 				String edges = output.readLine();
 				int count = Integer.parseInt(edges);
 				if (count > bestResultsCount) {
+					if (j == 1) {
+						Ashley += 1;
+					}
 					bestResultsCount = count;
 					bestResultsArray[i] = output.readLine();
 				}
@@ -29,9 +33,10 @@ public class FileConcatenator {
 		
 		PrintWriter out = new PrintWriter("outputs/testResults.out");
 		//Output the Strings into the out file
-		for(int i = 1; i < 10; i++) {
+		for(int i = 1; i < 622; i++) {
 			out.println(bestResultsArray[i]);
 		}
 		out.close();
+		System.out.print(Ashley);
 	}
 }
